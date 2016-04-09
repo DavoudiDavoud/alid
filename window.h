6b45a3a0773825708abe9eb1f2687783af3a8fc4
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-09 13:14:15 +0100

    Update window.h

--- window.h
+++ window.h
@@ -10,35 +10,30 @@
 
 #include "adcreader.h"
  
-// class definition 'Window'
 class Window : public QWidget
 {
-	// must include the Q_OBJECT macro for for the Qt signals/slots framework to work with this class
+	
 	Q_OBJECT
 
 public:
-	Window(); // default constructor - called when a Window is declared without arguments
+	Window(); 
 
 	~Window();
 
 	void timerEvent( QTimerEvent * );
 
-public slots:
-	void setFunc(double func);
 
-// internal variables for the window class
 private:
 
 	QwtPlot      *plot;
 	QwtPlotCurve *curve;
-	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
-	QHBoxLayout  *hLayout;  // horizontal layout
+	QHBoxLayout  *hLayout;  
 
 	static const int plotDataSize = 1000;
-	// data arrays for the plot
+
 	double xData[plotDataSize];
 	double yData[plotDataSize];
 	ADCreader *adcreader;
 };
 
-#endif // WINDOW_H
+#endif
