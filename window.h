abe35e7209784d3e9d69b93214f7c1066d31042e
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-09 13:07:59 +0100

    Update window.h

--- window.h
+++ window.h
@@ -1,15 +1,14 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
-//#include <qwt/qwt_thermo.h>
-#include <qwt/qwt_knob.h>
+
 #include <qwt/qwt_plot.h>
 #include <qwt/qwt_plot_curve.h>
 #include <qwt/qwt_text_label.h>
 
 #include <QBoxLayout>
 
- #include "adcreader.h"
+#include "adcreader.h"
  
 // class definition 'Window'
 class Window : public QWidget
@@ -29,26 +28,16 @@ public slots:
 
 // internal variables for the window class
 private:
-	QwtKnob      *knob;
-	QwtTextLabel *m_label;
-	QwtTextLabel *m1_label;
-//	QwtThermo    *thermo;
+
 	QwtPlot      *plot;
 	QwtPlotCurve *curve;
-
 	// layout elements from Qt itself http://qt-project.org/doc/qt-4.8/classes.html
-	QVBoxLayout  *vLayout;  // vertical layout
 	QHBoxLayout  *hLayout;  // horizontal layout
 
 	static const int plotDataSize = 1000;
-
 	// data arrays for the plot
 	double xData[plotDataSize];
 	double yData[plotDataSize];
-
-        double func;
-	int count;
-
 	ADCreader *adcreader;
 };
 
